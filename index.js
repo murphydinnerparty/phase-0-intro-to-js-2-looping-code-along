079edf4ed99b82d9f73cd367e928b66d94d07fc9
morgamo2 <mmoraleshoward@gmail.com>
2023-03-12 15:09:01 -0500

    completion of loop lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let peopleToGift =[];
+        for (let i = 0; i < names.length; i++){
+            peopleToGift.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+    return peopleToGift;
+}
+function countDown(number){
+   number = 10;
+   while (number > 0){
+       console.log(number--);
+   }
+ return console.log(number);  
+}
